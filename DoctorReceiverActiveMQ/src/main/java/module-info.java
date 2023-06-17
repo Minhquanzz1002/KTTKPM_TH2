ce79683e5799581a444feadf29e8d56030bd3252
Nguyễn Minh Quân <quannguyenminh1001@gmail.com>
2023-06-17 16:56:41 +0700

    complete UI receiver

--- DoctorReceiverActiveMQ/src/main/java/module-info.java
+++ DoctorReceiverActiveMQ/src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module vn.edu.iuh {
     requires javafx.controls;
     requires javafx.fxml;
-            
+
         requires org.controlsfx.controls;
                         requires org.kordamp.bootstrapfx.core;
-            
+
     opens vn.edu.iuh to javafx.fxml;
     exports vn.edu.iuh;
 }
