7e7dee5de0c3d5a90a5e7fe57e56d5faa81bbb91
Nguyễn Minh Quân <20098151.quan@student.iuh.edu.vn>
2023-06-23 12:19:15 +0700

    add handle selected listview

--- DoctorReceiverMQ/src/main/java/vn/edu/iuh/doctorreceivermq/DoctorController.java
+++ DoctorReceiverMQ/src/main/java/vn/edu/iuh/doctorreceivermq/DoctorController.java
@@ -2,14 +2,13 @@ package vn.edu.iuh.doctorreceivermq;
 
 import jakarta.xml.bind.JAXBException;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import org.apache.log4j.BasicConfigurator;
 import vn.edu.iuh.doctorreceivermq.data.Patient;
 import vn.edu.iuh.doctorreceivermq.helper.XMLConvert;
@@ -22,7 +21,7 @@ import java.net.URL;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
-public class DoctorController implements Initializable{
+public class DoctorController implements Initializable {
 
     @FXML
     private Button btnCall;
@@ -37,7 +36,7 @@ public class DoctorController implements Initializable{
     private TextField txtAddress;
 
     @FXML
-    private TextField txtCMND;
+    private TextField txtIdentityCardId;
 
     @FXML
     private TextArea txtContent;
@@ -48,63 +47,69 @@ public class DoctorController implements Initializable{
     @FXML
     private TextField txtName;
 
+    @FXML
+    public void handleMouseClick() {
+        Patient patient = lst.getSelectionModel().getSelectedItem();
+        txtId.setText(patient.getId() + "");
+        txtIdentityCardId.setText(patient.getIdentityCardId());
+        txtAddress.setText(patient.getAddress());
+        txtName.setText(patient.getName());
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        lst.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
-            XMLConvert<Patient> xmlConvert = new XMLConvert<Patient>(new Patient());
-            BasicConfigurator.configure();
-            Properties settings = new Properties();
-            settings.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-            settings.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
-            try {
-                Context ctx = new InitialContext(settings);
-                Object obj = ctx.lookup("ConnectionFactory");
-                ConnectionFactory factory = (ConnectionFactory) obj;
-                Destination destination = (Destination) ctx.lookup("dynamicQueues/thanthidet");
-                Connection connection =factory.createConnection("admin","admin");
-                connection.start();
-                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                MessageConsumer receiver = session.createConsumer(destination);
-                System.out.println("toi day");
-                receiver.setMessageListener(new MessageListener() {
-                    @Override
-                    public void onMessage(Message message) {
-                        try {
-                            System.out.println("toi day 2");
-                            if (message instanceof TextMessage) {
-                                TextMessage tm = (TextMessage) message;
-                                String txt = tm.getText();
-                                Patient patient = new Patient();
-                                patient = new XMLConvert<Patient>(patient).xmlToObject(txt);
-                                System.out.println(patient.toString());
 
-//                            lst.getItems().add(new Patient(1, "1", "quan", "1"));
-//                            lst.getItems().add("1");
-                                lst.refresh();
-                                Patient finalPatient = patient;
-                                Platform.runLater(() -> {
-                                    lst.getItems().add(finalPatient);
+        XMLConvert<Patient> xmlConvert = new XMLConvert<Patient>(new Patient());
+        BasicConfigurator.configure();
+        Properties settings = new Properties();
+        settings.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+        settings.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
+        try {
+            Context ctx = new InitialContext(settings);
+            Object obj = ctx.lookup("ConnectionFactory");
+            ConnectionFactory factory = (ConnectionFactory) obj;
+            Destination destination = (Destination) ctx.lookup("dynamicQueues/thanthidet");
+            Connection connection = factory.createConnection("admin", "admin");
+            connection.start();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer receiver = session.createConsumer(destination);
+            receiver.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    try {
+                        if (message instanceof TextMessage) {
+                            TextMessage tm = (TextMessage) message;
+                            String txt = tm.getText();
+                            Patient patient = new Patient();
+                            patient = new XMLConvert<Patient>(patient).xmlToObject(txt);
+                            System.out.println(patient.toString());
 
-                                });
-                                message.acknowledge();
-                            } else if (message instanceof ObjectMessage) {
-                                ObjectMessage om = (ObjectMessage) message;
-                                System.out.println(om);
-                            }
+                            lst.refresh();
+                            Patient finalPatient = patient;
+                            Platform.runLater(() -> {
+                                lst.getItems().add(finalPatient);
 
-                        } catch (JMSException e) {
-                            throw new RuntimeException(e);
-                        } catch (JAXBException e) {
-                            throw new RuntimeException(e);
+                            });
+                            message.acknowledge();
+                        } else if (message instanceof ObjectMessage) {
+                            ObjectMessage om = (ObjectMessage) message;
+                            System.out.println(om);
                         }
+
+                    } catch (JMSException e) {
+                        throw new RuntimeException(e);
+                    } catch (JAXBException e) {
+                        throw new RuntimeException(e);
                     }
-                });
-            } catch (NamingException e) {
-                throw new RuntimeException(e);
-            } catch (JMSException e) {
-                throw new RuntimeException(e);
-            }
+                }
+            });
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
